9af5cfd431cf5be9bc6580eb58b5d1c7698c7dd6
Mohd Anas <officialmohdanas786@gmail.com>
2025-11-30 22:42:16 +0530

    Include task router in main.py
    Removed placeholder comment and included task router.

--- app/main.py
+++ app/main.py
@@ -16,8 +16,6 @@ def root():
 def health():
     return {"status": "ok"}
 
-# include your actual logic
-app.include_router(task_router)
 
 @app.post("/quiz")
 async def receive_quiz(request: Request):
